9583fa65d004a226ed06233827d7635102040536
Strovertz - Gleison Pires <74078237+strovertz@users.noreply.github.com>
2022-11-09 11:26:29 -0300

    acrescentando navegacao entre paginas

--- projeto/src/main/java/trabalhopd/projeto/Main.java
+++ projeto/src/main/java/trabalhopd/projeto/Main.java
@@ -2,37 +2,65 @@ package trabalhopd.projeto;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.security.cert.PolicyNode;
+import java.util.Objects;
 
 public class Main extends Application {
 
     private static Stage stage;
 
     private static Scene mainScene;
+    public static Stage WINDOWS;
     private static Scene optionsScene;
+    private static int t;
 
+    /*public Scene createContent_1(){
+        // Conteúdo I
+        FXMLLoader fxmlData = new FXMLLoader(Main.class.getResource("data.fxml"));
+        mainScene = new Scene(fxmlData.load(), 1366, 768);
+        return mainScene;
+    }
+
+    public Scene createContent_2() throws IOException {
+
+        // Conteúdo II
+        FXMLLoader fxmlOptions = new FXMLLoader(Main.class.getResource("options.fxml"));
+        optionsScene = new Scene(fxmlOptions.load(), 1366, 768);
+        return optionsScene;
+    }*/
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws Exception{
+
+        WINDOWS = stage;
 
         FXMLLoader fxmlData = new FXMLLoader(Main.class.getResource("data.fxml"));
         mainScene = new Scene(fxmlData.load(), 1366, 768);
         FXMLLoader fxmlOptions = new FXMLLoader(Main.class.getResource("options.fxml"));
         optionsScene = new Scene(fxmlOptions.load(), 1366, 768);
-        stage.setTitle("LoginPage");
-        stage.setScene(mainScene);
-        stage.show();
 
+        WINDOWS.setScene(mainScene);
+
+        WINDOWS.setTitle("Tela Principal");
+
+        WINDOWS.show();
     }
-    public static void changeScreen(String scr){
-        switch (scr){
-            case "main":
-                stage.setScene(mainScene);
+    public static void change(int str){
+        switch (str){
+            case 1:
+                WINDOWS.setScene(mainScene);
+                WINDOWS.setTitle("Tela Principal");
+                WINDOWS.show();
                 break;
-            case "options":
-                stage.setScene(optionsScene);
+            case 2:
+                WINDOWS.setScene(optionsScene);
+                WINDOWS.setTitle("Tela Principal");
+                WINDOWS.show();
                 break;
         }
     }
--- projeto/src/main/java/trabalhopd/projeto/dataController.java
+++ projeto/src/main/java/trabalhopd/projeto/dataController.java
@@ -1,5 +1,6 @@
 package trabalhopd.projeto;
 
+import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -7,16 +8,20 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.time.chrono.MinguoChronology;
+
+import static javafx.application.Platform.exit;
+
 public class dataController {
 
 
     @FXML private TextField nome;
     @FXML private TextField Problema;
     @FXML private TextField turma;
+    @FXML public static Button NextPage;
 
 
     public void catchInfos(Event event){
-        Main.changeScreen("options");
         System.out.println(nome.getText());
         System.out.println(turma.getText());
         System.out.println(Problema.getText());
@@ -24,5 +29,13 @@ public class dataController {
         alert.setContentText("Olá " + nome.getText() + " " + "Turma: " + turma.getText() + " "  + "Apresenta: " + Problema.getText());
         alert.showAndWait();
 
+    }
+
+    public void NextPage(){
+        Main.change(2);
+    }
+
+    public void Exit(ActionEvent actionEvent) {
+        exit();
     }
 }
